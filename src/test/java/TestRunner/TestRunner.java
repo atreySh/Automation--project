f8f6571a9d05fbe8bc475aa03aad205fa0b9a08a
Atrey Pote <164036358+atreySh@users.noreply.github.com>
2025-08-12 13:50:41 +0530

    Update TestRunner.java

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,32 +1,53 @@
-package TestRunner;
+// package TestRunner;
 
-import org.junit.runner.RunWith;
+// import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-
+// import io.cucumber.junit.Cucumber;
+// import io.cucumber.junit.CucumberOptions;
 
 
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-	    features = {"src/test/resources/Features/flipkart.feature"},      // Path to feature files
-	    glue = {"Stepdefinitions", "Hooks"}, 
-	    		tags = "@tc003",
-	    		plugin = {
-	    		        "pretty",
-	    		        "html:target/cucumber-html-report",
-	    		        "json:target/cucumber.json",
-	    		        "rerun:target/rerun.txt",
-	    		        "TestRunner.CucumberExtentReportPlugin"
-	    		       	    		    },
+
+
+// @RunWith(Cucumber.class)
+// @CucumberOptions(
+// 	    features = {"src/test/resources/Features/flipkart.feature"},      // Path to feature files
+// 	    glue = {"Stepdefinitions", "Hooks"}, 
+// 	    		tags = "@tc003",
+// 	    		plugin = {
+// 	    		        "pretty",
+// 	    		        "html:target/cucumber-html-report",
+// 	    		        "json:target/cucumber.json",
+// 	    		        "rerun:target/rerun.txt",
+// 	    		        "TestRunner.CucumberExtentReportPlugin"
+// 	    		       	    		    },
 	    		
-	    		 monochrome = false
+// 	    		 monochrome = false
 	    
 	   
-	)
-	public class TestRunner  {
-	    // Empty class - the annotations drive the configuration
+// 	)
+// 	public class TestRunner  {
+// 	    // Empty class - the annotations drive the configuration
+// }
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = {"src/test/resources/Features/flipkart.feature"},  // Path to feature files
+    glue = {"Stepdefinitions", "Hooks"},
+    tags = "@tc003",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-html-report",
+        "json:target/cucumber.json",
+        "rerun:target/rerun.txt",
+        "TestRunner.CucumberExtentReportPlugin"
+    },
+    monochrome = false
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    // No code needed - inherited methods run the tests
 }
 
